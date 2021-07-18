02b8eda6c77e17ff3ed8c40b2f88d4d8c465ef4d
dueni1 <86540444+dueni1@users.noreply.github.com>
2021-07-18 14:30:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>indexpage</title>
+</head>
+<body>
+
+
+<li><a href="login.php">LOGIN</a></li>
+	<li><a href="register.php">REGSITER</a></li>
+
+
+
+<h1>you are welcome to indexpage</h1>
+
+<h2>proceed to login or register</h2>
+	
+</body>
+</html>
